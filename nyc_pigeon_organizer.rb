2613e3b5a9918aa7b4072c9d57455efcaaa317f7
Mary Farner <maryfarner@gmail.com>
2019-08-27 00:15:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,12 @@
 def nyc_pigeon_organizer(data)
   result = {}
-
+  
   data.each_key do |top_key|
     top_key.each_key do |low_key|
       low_key.collect do |name|
-        if !result[name] { result[name] = {} }
+        if !result[name] then
+          { result[name] = {} }
+        end
         result[name][top_key] = low_key
       end
     end
