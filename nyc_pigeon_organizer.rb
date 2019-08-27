b699d695bf673a3d2190a0f105b71ab3b1ccf90c
Mary Farner <maryfarner@gmail.com>
2019-08-27 00:21:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  
+  data.each_key do |top_key|
+    data[top_key].each_key do |low_key|
+      data[top_key][low_key].collect do |name|
+        if !result[name] then
+          result[name] = {}
+        end
+        if !result[name][top_key] then
+          result[name][top_key] = []
+        end
+        
+        result[name][top_key].push(low_key.to_s)
+    
+      end
+    end
+  end
+ 
+  result
 end
