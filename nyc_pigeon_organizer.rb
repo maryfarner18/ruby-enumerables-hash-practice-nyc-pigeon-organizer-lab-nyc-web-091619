f17d9fb3b23f1f766f6724a2f48f9f8dd0c34bea
Mary Farner <maryfarner@gmail.com>
2019-08-27 00:10:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,10 @@
 def nyc_pigeon_organizer(data)
   result = {}
-  data[:gender][:male].collect do |name|
-    result[name] = {}
+  data[:gender].each_key do |key|
+    key.collect do |name|
+      result[name] = {}
+      result[name][:gender] = key
+    end
     result[name][:gender] = "male"
   end
   
